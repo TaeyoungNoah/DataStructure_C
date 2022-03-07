c146bb575d71752e5c361b98f3b1217bd91aece3
TaeyoungNoah <tyty09171007@gmail.com>
2022-03-07 10:49:23 +0900

    feat: chapter 06, ArrayBaseStack

---
+++ Cpractice/book/chap06/ArrayBaseStack/ArrayBaseStack.h
@@ -0,0 +1,24 @@
+#ifndef __AB_STACK_H__
+#define __AB_STACK_H__
+
+#define TRUE 1
+#define FALSE 0
+#define STACK_LEN 100
+
+typedef int Data;
+
+typedef struct _arrayStack {
+    Data stackArr[STACK_LEN];
+    int topIndex;
+} ArrayStack;
+
+typedef ArrayStack Stack;
+
+void StackInit(Stack *pstack); // 스택 초기화
+int SIsEmpty(Stack *pstack); // 스택이 비었는지 확인
+
+void SPush(Stack *pstack, Data data); // push
+Data SPop(Stack *pstack); // pop
+Data SPeek(Stack *pstack); // peek
+
+#endif
