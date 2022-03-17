71aff7af92f4b9d14d9fd4c3628000f6ff05bdd6
TaeyoungNoah <tyty09171007@gmail.com>
2022-03-17 22:42:27 +0900

    comment(self): chapter 03, ArrayListHW_char

--- Cpractice/Self/chap03/ArrayListHW_char/ListMain.c
+++ Cpractice/Self/chap03/ArrayListHW_char/ListMain.c
@@ -8,14 +8,20 @@ int main(void) {
     List list;
     ListInit(&list);
 
-    //5개 데이터 저장
+    // 10개 데이터 저장
     LInsert(&list, 'a');
     LInsert(&list, 'B');
+    LInsert(&list, 'B');
     LInsert(&list, 'c');
-    LInsert(&list, 'D');
+    LInsert(&list, 'd');
+    LInsert(&list, 'E');
     LInsert(&list, 'e');
+    LInsert(&list, 'F');
+    LInsert(&list, 'G');
+    LInsert(&list, 'h');
 
     //저장된 데이터의 전체 출력
+    printf("=== 삭제 전 데이터 ===");
     printf("현재 데이터의 수 : %d\n", LCount(&list));
     if(LFirst(&list, &data)) {
         printf("%c ", data);
@@ -38,6 +44,30 @@ int main(void) {
     }
 
     //삭제 후 남은 데이터 전체 출력
+    printf("=== 'B' 삭제 후 남은 데이터 출력 ===");
+    printf("현재 데이터의 수 : %d\n", LCount(&list));
+    if(LFirst(&list, &data)) {
+        printf("%c ", data);
+
+        while(LNext(&list, &data)) {
+            printf("%c ", data);
+        }
+    }
+    printf("\n\n");
+
+    //char 'e'를 탐색하여 모두 삭제
+    if(LFirst(&list, &data)) {
+        if(data == 'e')
+            LRemove(&list);
+
+        while(LNext(&list, &data)) {
+            if(data == 'e')
+                LRemove(&list);
+        }
+    }
+
+    //삭제 후 남은 데이터 전체 출력
+    printf("=== 'E' 삭제 후 남은 데이터 출력 ===");
     printf("현재 데이터의 수 : %d\n", LCount(&list));
     if(LFirst(&list, &data)) {
         printf("%c ", data);
